c725e13a785c4a27a4e6fae8618d36a661428e7f
Wil <wfabian.ardila@gmail.com>
2024-06-04 22:50:36 +1200

    refactor: Remove useless implementations

--- bombed_maze_factory.py
+++ bombed_maze_factory.py
@@ -1,6 +1,5 @@
 """This module defines a maze with bombs"""
 
-from maze import Maze
 from maze_factory import MazeFactory
 from maze_site import  Room, RoomWithABomb, Wall, BombedWall
 
@@ -12,11 +11,3 @@ class BombedMazeFactory(MazeFactory):
 
     def make_room(self, room_number: int) -> Room:
         return RoomWithABomb(room_number)
-
-    #pylint: disable=useless-parent-delegation
-    # Only 2 methods above are implemented for educational purposes
-    def make_maze(self) -> Maze:
-        return super().make_maze()
-
-    def make_door(self, room_1: Room, room_2: Room):
-        return super().make_door(room_1, room_2)
--- enchanted_maze_factory.py
+++ enchanted_maze_factory.py
@@ -1,8 +1,7 @@
 """This module defines a Enchanted Maze Factory"""
 
-from maze import Maze
 from maze_factory import MazeFactory
-from maze_site import Room, EnchantedRoom, Spell, DoorNeedingSpell, Wall
+from maze_site import Room, EnchantedRoom, Spell, DoorNeedingSpell
 
 class EnchantedMazeFactory(MazeFactory):
     """Defines an Enchanted Maze"""
@@ -13,14 +12,6 @@ class EnchantedMazeFactory(MazeFactory):
 
     def make_door(self, room_1, room_2):
         return DoorNeedingSpell(room_1, room_2)
-
-    #pylint: disable=useless-parent-delegation
-    # Only 2 methods above are implemented for educational purposes
-    def make_wall(self) -> Wall:
-        return super().make_wall()
-
-    def make_maze(self) -> Maze:
-        return super().make_maze()
 
     def _cast_spell(self) -> Spell:
         return Spell()
